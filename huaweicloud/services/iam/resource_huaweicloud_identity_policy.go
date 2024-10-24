9fb85a57ff3332ef2881ae067362a6e41c8bb127
Jason Zhang <jasonzhang9309@163.com>
2024-10-24 14:56:45 +0800

    feat(iam): support updating identity policy

--- huaweicloud/services/iam/resource_huaweicloud_identity_policy.go
+++ huaweicloud/services/iam/resource_huaweicloud_identity_policy.go
@@ -2,6 +2,8 @@ package iam
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
@@ -15,11 +17,15 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
 )
 
-var policyNonUpdatableParams = []string{"name", "policy_document", "path", "description"}
+var policyNonUpdatableParams = []string{"name", "path", "description"}
 
 // @API IAM POST /v5/policies
 // @API IAM GET /v5/policies/{policy_id}
 // @API IAM DELETE /v5/policies/{policy_id}
+// @API IAM POST /v5/policies/{policy_id}/versions
+// @API IAM GET /v5/policies/{policy_id}/versions
+// @API IAM DELETE /v5/policies/{policy_id}/versions/{version_id}
+// @API IAM POST /v5/policies/{policy_id}/versions/{version_id}/set-default
 func ResourceIdentityPolicy() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceIdentityPolicyCreate,
@@ -150,7 +156,7 @@ func resourceIdentityPolicyRead(_ context.Context, d *schema.ResourceData, meta 
 
 	policy := utils.PathSearch("policy", getPolicyRespBody, nil)
 	if policy == nil {
-		return diag.Errorf("error getting IAM trust policy: policy is not found in API response")
+		return diag.Errorf("error getting IAM identity policy: policy is not found in API response")
 	}
 
 	mErr := multierror.Append(nil,
@@ -166,14 +172,162 @@ func resourceIdentityPolicyRead(_ context.Context, d *schema.ResourceData, meta 
 	)
 
 	if err := mErr.ErrorOrNil(); err != nil {
-		return diag.Errorf("error setting IAM trust policy fields: %s", err)
+		return diag.Errorf("error setting IAM identity policy fields: %s", err)
 	}
 
 	return nil
 }
 
-func resourceIdentityPolicyUpdate(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+func getIdentityPolicyVersions(client *golangsdk.ServiceClient, d *schema.ResourceData) ([]interface{}, error) {
+	getPolicyVersionHttpUrl := "v5/policies/{policy_id}/versions"
+	getPolicyVersionPath := client.Endpoint + getPolicyVersionHttpUrl
+	getPolicyVersionPath = strings.ReplaceAll(getPolicyVersionPath, "{policy_id}", d.Id())
+	getPolicyVersionOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	var policyVersions []interface{}
+	for {
+		getPolicyVersionResp, err := client.Request("GET", getPolicyVersionPath, &getPolicyVersionOpt)
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving IAM identity policy versions: %s", err)
+		}
+		getPolicyVersionRespBody, err := utils.FlattenResponse(getPolicyVersionResp)
+		if err != nil {
+			return nil, err
+		}
+
+		policyVersionsTemp := utils.PathSearch("versions", getPolicyVersionRespBody, nil)
+		if policyVersionsTemp == nil {
+			return nil, fmt.Errorf("error getting IAM identity policy versions: versions is not found in API response")
+		}
+
+		policyVersions = append(policyVersions, policyVersionsTemp.([]interface{})...)
+
+		marker := utils.PathSearch("page_info.next_marker", getPolicyVersionRespBody, nil)
+		if marker == nil {
+			break
+		}
+		getPolicyVersionPath = getPolicyVersionPath + fmt.Sprintf("?marker=%s", marker)
+	}
+
+	return policyVersions, nil
+}
+
+func removeIdentityPolicyVersion(client *golangsdk.ServiceClient, d *schema.ResourceData, versionID string) error {
+	deletePolicyVersionHttpUrl := "v5/policies/{policy_id}/versions/{version_id}"
+	deletePolicyVersionPath := client.Endpoint + deletePolicyVersionHttpUrl
+	deletePolicyVersionPath = strings.ReplaceAll(deletePolicyVersionPath, "{policy_id}", d.Id())
+	deletePolicyVersionPath = strings.ReplaceAll(deletePolicyVersionPath, "{version_id}", versionID)
+	deletePolicyVersionOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+	}
+
+	_, err := client.Request("DELETE", deletePolicyVersionPath, &deletePolicyVersionOpt)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func buildAddPolicyVersionBodyParams(d *schema.ResourceData) map[string]interface{} {
+	bodyParams := map[string]interface{}{
+		"policy_document": d.Get("policy_document").(string),
+		"set_as_default":  true,
+	}
+	return bodyParams
+}
+
+func addIdentityPolicyVersion(client *golangsdk.ServiceClient, d *schema.ResourceData) error {
+	addPolicyVersionHttpUrl := "v5/policies/{policy_id}/versions"
+	addPolicyVersionPath := client.Endpoint + addPolicyVersionHttpUrl
+	addPolicyVersionPath = strings.ReplaceAll(addPolicyVersionPath, "{policy_id}", d.Id())
+	addPolicyVersionOpt := golangsdk.RequestOpts{
+		KeepResponseBody: true,
+		JSONBody:         utils.RemoveNil(buildAddPolicyVersionBodyParams(d)),
+	}
+
+	_, err := client.Request("POST", addPolicyVersionPath, &addPolicyVersionOpt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handleAddVersionError409(err error) (bool, error) {
+	if err == nil {
+		return false, nil
+	}
+	if errCode, ok := err.(golangsdk.ErrUnexpectedResponseCode); ok && errCode.Actual == 409 {
+		var apiError interface{}
+		if jsonErr := json.Unmarshal(errCode.Body, &apiError); jsonErr != nil {
+			return false, jsonErr
+		}
+
+		errCode := utils.PathSearch("error_code", apiError, nil)
+		if apiError == nil {
+			return false, err
+		}
+
+		// PAP5.0028: versions per policy limit exceeded
+		if errCode == "PAP5.0028" {
+			return true, err
+		}
+	}
+	return false, err
+}
+
+func getEarliestVersionID(client *golangsdk.ServiceClient, d *schema.ResourceData) (string, error) {
+	versions, err := getIdentityPolicyVersions(client, d)
+	if err != nil {
+		diag.Errorf("error retrieving the versions of IAM identity policy(%s): %s", d.Id(), err)
+	}
+
+	if len(versions) == 0 {
+		return "", fmt.Errorf("the list of policy versions is empty")
+	}
+
+	return utils.PathSearch("[-1].version_id", versions, "").(string), nil
+}
+
+func resourceIdentityPolicyUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cfg := meta.(*config.Config)
+	region := cfg.GetRegion(d)
+	client, err := cfg.NewServiceClient("iam_no_version", region)
+	if err != nil {
+		return diag.Errorf("error creating IAM client: %s", err)
+	}
+	policyID := d.Id()
+
+	if d.HasChange("policy_document") {
+		err = addIdentityPolicyVersion(client, d)
+		if err != nil {
+			removeVersion, err := handleAddVersionError409(err)
+			// if get a "versions per policy limit exceeded" error
+			// remove a version and try again
+			if removeVersion {
+				versionID, err := getEarliestVersionID(client, d)
+				if err != nil {
+					return diag.FromErr(err)
+				}
+
+				err = removeIdentityPolicyVersion(client, d, versionID)
+				if err != nil {
+					diag.Errorf("error removing the earliest version of IAM identity policy(%s): %s", policyID, err)
+				}
+
+				err = addIdentityPolicyVersion(client, d)
+				if err != nil {
+					return diag.Errorf("error adding a new version of IAM identity policy(%s): %s", policyID, err)
+				}
+			} else {
+				return diag.Errorf("error adding a new version of IAM identity policy(%s): %s", policyID, err)
+			}
+		}
+	}
+	return resourceIdentityPolicyRead(ctx, d, meta)
 }
 
 func resourceIdentityPolicyDelete(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
